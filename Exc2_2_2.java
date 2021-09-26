77dd6356af6edb7773201d9659c884719f7e2825
ajrpTec <ajrp@tec.dk>
2021-09-26 17:04:45 +0200

    exc222

---
+++ Exc2_2_2.java
@@ -0,0 +1,48 @@
+public class Exc2_2_2{
+
+  public static void main(String[] args){
+
+    String values = "EASYQUESTION";
+    String[] array = values.split("(?!^)");
+    Comparable[] list = array;
+
+    mergeSort(list);
+
+    for(Comparable s : list) System.out.print(s);System.out.println("");
+  }
+
+
+  private static Comparable[] aux;
+
+  public static void mergeSort(Comparable[] a){
+    aux = new Comparable[a.length];
+    mergeSort(a, 0, a.length - 1);
+  }
+
+  public static void mergeSort(Comparable[] a, int lo, int hi){
+    if(hi <= lo) return;
+    int mid = lo + (hi - lo)/2;
+    mergeSort(a, lo, mid);
+    mergeSort(a, mid+1, hi);
+    merge(a,lo,mid,hi);
+  }
+
+  public static void merge(Comparable[] a, int lo, int mid, int hi){
+
+    System.out.println("merge(a," + lo + "," + mid + "," + hi + ")");
+    int i = lo, j = mid +1;
+    for(int k = lo ; k <= hi ; k++)
+      aux[k] = a[k];
+    for(int k = lo; k <= hi ; k++)
+      if      (i > mid)             a[k] = aux[j++];
+      else if (j > hi)              a[k] = aux[i++];
+      else if (less(aux[j],aux[i])) a[k] = aux[j++];
+      else                          a[k] = aux[i++];
+          for(Comparable s : a) System.out.print(s);System.out.println("");
+  }
+
+  public static boolean less(Comparable v, Comparable w){
+    return v.compareTo(w) < 0;
+  }
+
+}
